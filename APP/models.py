bf488df23b711895dfa4287b6d57999c02a5245c
challavinay251 <121007692+challavinay251@users.noreply.github.com>
2023-01-02 22:03:48 +0530

    Update models.py
    firstcommit

--- APP/models.py
+++ APP/models.py
@@ -1,3 +1,402 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+from django.db import models
+class four_1(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class three_1(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class two_1(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+
+from django.db import models
+class one_1_a(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+
+from django.db import models
+class one_1_b(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+
+from django.db import models
+class four_2(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class three_2(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class two_2(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class one_2_a(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+from django.db import models
+class one_2_b(models.Model):
+ DAY=models.CharField(max_length=36);
+ FP=models.CharField(max_length=36);
+ SP=models.CharField(max_length=36);
+ BR=models.CharField(max_length=36);
+ TP=models.CharField(max_length=36);
+ FRP=models.CharField(max_length=36);
+ LP=models.CharField(max_length=36);
+ FTP=models.CharField(max_length=36);
+ STP=models.CharField(max_length=36);
+def __srt__(self):
+  return self.DAY+self.FP+self.SP+self.BR+self.TP+self.FRP+self.LP+self.FTP+self.STP ;
+
+
+from django.db import models
+class Saithirumal_1_1_a(models.Model):
+ DAY=models.CharField(max_length=22);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Saithirumal_1_1_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Saithirumal_2_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Saithirumal_2_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Naveen_1_2_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Naveen_1_2_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+
+class Naveen_2_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Narender_1_1_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Narender_1_1_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Narender_4_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Zulfiquar_1_1_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Zulfiquar_1_1_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Zulfiquar_1_2_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Zulfiquar_1_2_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Zulfiquar_2_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Babu_2_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Babu_2_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Babu_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Babu_3_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Babu_4_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Subharao_1_1_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Subharao_1_1_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Subharao_2_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Subharao_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Subharao_3_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Anil_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Anil_3_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Anil_4_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Subhan_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Subhan_3_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Subhan_4_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Venkat_1_1_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Venkat_1_1_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Venkat_1_2_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Venkat_1_2_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Cassian_1_2_a(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Cassian_1_2_b(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Santhosh_3_2(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+class Santhosh_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
+
+
+class Devaraj_3_1(models.Model):
+ DAY=models.CharField(max_length=25);
+ Classtime=models.CharField(max_length=25);
+def __srt__(self):
+  return self.DAY+self.Classtime;
